93300bed93d8c157bbc57f5ef860210d9e889e18
zeroshine <as790726@gmail.com>
2013-10-08 23:14:01 +0800

    v1.0

---
+++ src/painter/Graphics.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package painter;
+
+/**
+ *
+ * @author zeroshine
+ */
+class Graphics {
+    
+}
